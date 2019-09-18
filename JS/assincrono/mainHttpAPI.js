6ed93815d5a7cd2600a616c062d6e867a5d4fa75
Lucas Mauro <lucas_de_mauro@hotmail.com>
2019-09-18 09:32:30 -0300

    JS - Todos e metodos assincronos

---
+++ JS/assincrono/mainHttpAPI.js
@@ -0,0 +1,11 @@
+// Requisições AJAX
+var xhr = new XMLHttpRequest();
+
+xhr.open('GET', 'https://api.github.com/users/LucasMSnts');
+xhr.send(null);
+
+xhr.onreadystatechange = function() {
+    if (xhr.readyState === 4) {
+        console.log(JSON.parse(xhr.responseText));
+    }
+}
