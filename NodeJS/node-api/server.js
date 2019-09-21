7ce104d01490b46145f94c92f8583e9069314202
Lucas Mauro <lucas_de_mauro@hotmail.com>
2019-09-20 23:11:27 -0300

    NodeJS - Conectando DB e criando model de produto

--- NodeJS/node-api/server.js
+++ NodeJS/node-api/server.js
@@ -3,11 +3,32 @@
 // npm run dev
  
 const express = require('express');
+const mongoose = require('mongoose');
+const requireDir = require('require-dir');
 
+// Iniciando o App
 const app = express();
 
+//Iniciando o DB
+mongoose.connect(
+    'mongodb://192.168.99.100:27017/nodeapi', // usando o Docker Toolbox
+    { useNewUrlParser: true, useUnifiedTopology: true }
+);
+// Sem o require-dir, terá que fazer um por um
+//ex.: require('./src/models/Product');
+requireDir('./src/models');
+
+const Product = mongoose.model('Product');
+
+//Primeira rota
 app.get('/', (req, res) => {
-    res.send('Helo World!');
+    Product.create({
+        title: 'React Native',
+        description: 'Build native apps with React',
+        url: 'http://github.com/facebook/react-native'
+    });
+    
+    return res.send('Helo World!');
 });
 
 app.listen(3001);
