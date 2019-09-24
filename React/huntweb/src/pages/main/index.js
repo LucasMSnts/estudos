969fde99343bb2be9437b9f9135185ed1e3b1bf3
Lucas Mauro <lucas_de_mauro@hotmail.com>
2019-09-24 10:40:02 -0300

    React - Listando produtos

--- React/huntweb/src/pages/main/index.js
+++ React/huntweb/src/pages/main/index.js
@@ -1,6 +1,8 @@
 import React, { Component } from "react";
 import api from "../../services/api";
 
+import './styles.css';
+
 export default class Main extends Component {
     state = {
         products: [],
@@ -18,11 +20,18 @@ export default class Main extends Component {
     };
     
     render() {
+        const { products } = this.state;
+
         //return <h1>Contagem de produtos: {this.state.products.length}</h1>;
         return (
             <div className="product-list">
-                {this.state.products.map(product => (
-                    <h2 key={product._id}>{product.title}</h2>
+                {products.map(product => (
+                    <article key={product._id}>
+                        <strong>{product.title}</strong>
+                        <p>{product.description}</p>
+
+                        <a href="">Acessar</a>
+                    </article>
                 ))}
             </div>
         );
